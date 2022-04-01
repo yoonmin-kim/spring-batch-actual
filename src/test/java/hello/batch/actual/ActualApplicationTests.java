0cea8dbab10f42efc95456e368ac32a521cff7c9
yoonmin-kim <rladbsals23@gmail.com>
2022-04-01 21:22:37 +0900

    Initial commit

---
+++ src/test/java/hello/batch/actual/ActualApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.batch.actual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
